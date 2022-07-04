f027842810991128cfe0a250b9b4dd62101d53f2
Alohadancemeow <nawongsak@gmail.com>
2022-07-04 17:15:25 +0700

    👎 😮‍💨

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,44 @@
-import type { NextPage } from 'next'
+import React from 'react';
+import axios from 'axios';
 
-const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+
+import VideoCard from '../compoents/VideoCard';
+import NoResults from '../compoents/NoResults';
+
+interface IProps {
+  videos: Video[];
 }
 
-export default Home
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        ))
+        : <NoResults text={`No Videos`} />}
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  // let response = await axios.get(`${BASE_URL}/api/post`);
+  let response = await axios.get(`http://localhost/api/post`);
+
+  if (topic) {
+    // response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+    response = await axios.get(`http://localhost/api/discover/${topic}`);
+  }
+
+  return {
+    props: { videos: response.data },
+  };
+};
